0fe52c2e5c51517d8fd47a542126ab7956baacc5
Strylor <Puresyn@users.noreply.github.com>
2024-02-12 13:24:11 -0600

    Updated instance abbreviations

--- KMInstanceTools.lua
+++ KMInstanceTools.lua
@@ -14,9 +14,9 @@ local InstanceTools = core.InstanceTools
 -- Challenge Dungeon Instance Abbreviations.
 -- Must be manually maintained.
 --------------------------------
-local mapAbbrTable = {
-    [463] = "DoI-F",    -- Dawn of the Infinite: Galakrond's Fall
-    [464] = "DoI-R",    -- Dawn of the Infinite: Murozond's Rise
+local instanceAbbTable = {
+    [463] = "FALL",    -- Dawn of the Infinite: Galakrond's Fall
+    [464] = "RISE",    -- Dawn of the Infinite: Murozond's Rise
     [244] = "AD",       -- Atal'Dazar
     [248] = "WCM",      -- Waycrest Manor
     [199] = "BRH",      -- Black Rook Hold
@@ -27,7 +27,7 @@ local mapAbbrTable = {
 
 -- conversion from mapid to abbreviation
 function InstanceTools:GetAbbr(mapId --[[int]])
-    local mapAbbr = mapAbbrTable[mapId]
-    if (not mapAbbr) then mapAbbr = "---" end
-    return mapAbbr
+    local a = instanceAbbTable[mapId]
+    if (not a) then a = "---" end
+    return a
 end
