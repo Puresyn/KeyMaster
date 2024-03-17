db918a661e178d24deb9a135af73c20775c60600
Strylor <Puresyn@users.noreply.github.com>
2024-03-16 22:36:48 -0500

    Changed "No Addon" text to icon for Open Raid integration

--- Core/UI/PartyFrame.lua
+++ Core/UI/PartyFrame.lua
@@ -236,12 +236,19 @@ function PartyFrame:CreatePartyDataFrame(parentFrame)
     PlayerClassText:SetPoint("TOPLEFT", _G["KM_PlayerName"..playerNumber], "BOTTOMLEFT", 0, 0)
 
     -- Player does not have the addon
-    local NoAddonText = dataFrame:CreateFontString("KM_NoAddon"..playerNumber, "OVERLAY", "KeyMasterFontBig")
+    --[[ local NoAddonText = dataFrame:CreateFontString("KM_NoAddon"..playerNumber, "OVERLAY", "KeyMasterFontBig")
     local font, fontSize, flags = NoAddonText:GetFont()
     NoAddonText:SetTextColor(0.6, 0.6, 0.6, 1)
     NoAddonText:SetPoint("BOTTOMLEFT", dataFrame, "BOTTOMLEFT", 10, 4)
     NoAddonText:SetText(KM_ADDON_NAME.." "..KeyMasterLocals.PARTYFRAME["NoAddon"].text)
-    NoAddonText:Hide()
+    NoAddonText:Hide() ]]
+
+    local noAddonIcon = dataFrame:CreateTexture("KM_NoAddon"..playerNumber, "OVERLAY")
+    noAddonIcon:SetSize(32, 32)
+    noAddonIcon:SetPoint("LEFT", dataFrame, "LEFT", 160, 0)
+    noAddonIcon:SetTexture("Interface/Addons/KeyMaster/Assets/Images/No-KM-Icon")
+    noAddonIcon:SetAlpha(0.2)
+    noAddonIcon:Hide()
 
     -- Player is offline
     local OfflineText = dataFrame:CreateFontString("KM_Player"..playerNumber.."Offline", "OVERLAY", "KeyMasterFontBig")
