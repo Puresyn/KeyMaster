9fa5efb9b8bc63853296fea8bc6c23aa652e8207
socmonkey05 <socman83@gmail.com>
2024-03-24 16:52:08 -0500

    moved dungeon journal functions into their own file.  Not being used yet.

--- Core/UI/PlayerFrame.lua
+++ Core/UI/PlayerFrame.lua
@@ -18,42 +18,6 @@ local function shortenDungeonName(fullDungeonName)
     end
 end
 
--- NOT SURE IF THIS WORKS
-local function getInstances(tier)
-    EJ_SelectTier(tier); -- sets dungeon journal data to current tier data
-    local instances = {}
-    local dataIndex = 1;
-	local instanceID, name, description, _, buttonImage, _, _, _, link, _, mapID = EJ_GetInstanceByIndex(dataIndex, false);
-
-	while instanceID ~= nil do
-		tinsert(instances,
-        {
-			instanceID = instanceID,
-			name = name,
-			description = description,
-			buttonImage = buttonImage,
-			link = link,
-			mapID = mapID,
-		});
-
-		dataIndex = dataIndex + 1;
-		instanceID, name, description, _, buttonImage, _, _, _, link, _, mapID = EJ_GetInstanceByIndex(dataIndex, false);
-	end
-
-    return instances
-end
-
--- NOT SURE IF THIS WORKS
-local function showDungeonJournal()
-    local mythicPlusDifficultiyId = 8
-    local currentTier = EJ_GetCurrentTier()
-    local instances = getInstances(currentTier)
-    if (not EncounterJournal_OpenJournal) then
-        UIParentLoadAddOn('Blizzard_EncounterJournal')
-    end
-    EncounterJournal_OpenJournal(mythicPlusDifficultiyId, instances[1].instanceID) -- Throws blizzard only action error   
-end
-
 --[[ local function portalButton_buttonevent(self, event)
     local spellNameToCheckCooldown = self:GetParent():GetAttribute("portalSpellName")
     local start, dur, _ = GetSpellCooldown(spellNameToCheckCooldown);
