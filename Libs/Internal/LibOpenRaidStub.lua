937ff3e1521d5b0c57df4c8b10d9296026829e42
Strylor <Puresyn@users.noreply.github.com>
2024-04-04 10:53:50 -0500

    Commit Rollback

--- Libs/Internal/LibOpenRaidStub.lua
+++ Libs/Internal/LibOpenRaidStub.lua
@@ -50,12 +50,6 @@ function openRaidStub:SendCommData(data, flags, bIgnoreQueue)
         -- end
     else
         if (IsInGroup() and not IsInRaid()) then --in party only
-                local channel
-            if IsInGroup(LE_PARTY_CATEGORY_HOME)  then
-                channel = "PARTY"
-            else
-                return
-            end
             local channel = IsInGroup(LE_PARTY_CATEGORY_INSTANCE) and "INSTANCE_CHAT" or "PARTY"
             KM_OpenRaidStub:TransmitOpenRaidRequest(dataEncoded, channel)
         end
