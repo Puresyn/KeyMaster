6bf5ce8fe88d4056fc44d155a8759f61f049bc9a
Strylor <Puresyn@users.noreply.github.com>
2024-06-15 09:34:43 -0500

    Added zhCN localization

--- Core/Localization/Localization.lua
+++ Core/Localization/Localization.lua
@@ -64,6 +64,9 @@ elseif (langPref == "zhTW") then
 elseif (langPref == "koKR") then
     -- Localization.koKR.lua
     KeyMasterLocals = KM_Localization_koKR
+elseif (langPref == "zhCN") then
+    -- Localization.koKR.lua
+    KeyMasterLocals = KM_Localization_zhCN
 else -- Default
     -- Localization.enUS.lua
     KeyMasterLocals = KM_Localization_enUS
