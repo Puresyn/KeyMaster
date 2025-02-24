948911e2cb319ca20d7255f7f0172d808316164b
Strylor <purecynergy@gmail.com>
2025-02-23 20:27:49 -0600

    Updated what's new patch notese.

--- Core/UI/WhatsNew.lua
+++ Core/UI/WhatsNew.lua
@@ -33,8 +33,9 @@ local function setWhatsNewContent(parent)
                 <br/>
                 <h2>|cff]]..h2Color..[[Updates:|r</h2>
                 <br/>
-                <p>]]..textBullet..[[Adjusted run threshold calculations (+1,2,3 etc).</p>          
+                <p>]]..textBullet..[[Updates for TWW S2.</p>
                 <br/>
+                <p>]]..textBullet..[[Will be loosing CN translation next release due to lack of translator participation.</p>          
                 <br/>
                 <h2>|cff]]..h2Color..[[Fixes:|r</h2>
                 <br/>
