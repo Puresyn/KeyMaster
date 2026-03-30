a407579bffa6f06420c5c496c2a230f92bcefb41
socmonkey05 <socman83@gmail.com>
2026-03-30 14:07:16 -0500

    Fix various issues including tainted strings and ensure numeric values are handled correctly

--- Libs/Internal/DungeonTools.lua
+++ Libs/Internal/DungeonTools.lua
@@ -216,7 +216,7 @@ function DungeonTools:GetCurrentSeason(retryCount)
     if retryCount == nil then retryCount = 0 end
     local maxRetryCount = 5    
     
-    local seasonNumber = C_MythicPlus.GetCurrentSeason()
+    local seasonNumber = tonumber(C_MythicPlus.GetCurrentSeason())
     if seasonNumber ~= nil and seasonNumber ~= -1 then
         currentSeason = seasonNumber -- stores locally to prevent multiple api calls
         return currentSeason
@@ -246,7 +246,7 @@ function DungeonTools:GetCurrentSeasonMaps()
        mapTable[id] = {
             ["id"] = id,
             ["name"] = name,
-            ["timeLimit"] = timeLimit,
+            ["timeLimit"] = tonumber(timeLimit) or 0,
             ["texture"] = texture,
             ["backgroundTexture"] = backgroundTexture
        }
@@ -635,10 +635,10 @@ function DungeonTools:ChallengeModeCompletionInfo()
 
     --if KeyMaster:GetTableLength(completionData) > 0 then
         C_Timer.After(3, function()
-            local mapName = DungeonTools:GetMapName(info.mapChallengeModeID)
+            local mapName = DungeonTools:GetMapName(tonumber(info.mapChallengeModeID))
             local timeStatus
             if info.onTime then timeStatus = "a timed" else timeStatus = "an untimed" end
-            local plusText = "+"..tostring(info.keystoneUpgradeLevels)
+            local plusText = "+"..tostring(tonumber(info.keystoneUpgradeLevels) or 0)
             if not mapName then KeyMaster:_ErrorMsg("GetChallengeCompletionInfo","DungeonTools", "Could not find mapID "..tostring(info.mapChallengeModeID)) 
                 return
             end
