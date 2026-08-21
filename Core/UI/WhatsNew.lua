64effc4fec20ca8b6b1f2c54309363d9245f65f9
socmonkey05 <jcorrington@stabilityit.net>
2026-08-21 02:46:59 -0500

    Add new season 2 dungeons and portals, along with localization and update patch notes

--- Core/UI/WhatsNew.lua
+++ Core/UI/WhatsNew.lua
@@ -33,7 +33,7 @@ local function setWhatsNewContent(parent)
                 <br/>
                 <h2>|cff]]..h2Color..[[Updates:|r</h2>
                 <br/>
-                <p>]]..textBullet..[[None</p>
+                <p>]]..textBullet..[[Added new season 2 dungeons and portals.</p>
                 <br/>
                 <br/>
                 <h2>|cff]]..h2Color..[[Fixes:|r</h2>
